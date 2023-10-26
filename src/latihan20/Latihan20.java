585d5e86f40cdc9a937ca37a8baf73609a710d96
rriffa <145914122+rriffa@users.noreply.github.com>
2023-10-26 13:16:38 +0700

    Update Latihan20.java

--- src/latihan20/Latihan20.java
+++ src/latihan20/Latihan20.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
  Nama :Ma'rifatu Khirzah
  NIM :22205025
  Prodi :Teknik Informatika
+ Deskripsi :Menampilkan program Perhitungan target saldo tabungan
  */
 public class Latihan20 {
 
